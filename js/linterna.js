c67c4cf5390567d9f5322d9fe5ceae4097eb9cc3
Paz Spera <mspera03@gmail.com>
2023-03-02 20:30:48 -0300

    JS

--- js/linterna.js
+++ js/linterna.js
@@ -1,2 +1,35 @@
 let estadoLinterna = false;
 
+let sonidoBatman = document.querySelector("#bati_encendido");
+let sonidoBoton = document.querySelector("#bati_click");
+
+let batman = document.querySelector("#batman");
+let boton = document.querySelector("#bati_boton");
+
+boton.addEventListener("click", () => {
+  controlarBatman();
+});
+
+function controlarBatman() {
+  if (estadoLinterna === false) {
+    estadoLinterna = true;
+    sonidoLinterna();
+    batman.classList.add("batman_activo");
+  } else {
+    estadoLinterna = false;
+    sonidoLinterna();
+    batman.classList.remove("batman_activo");
+  }
+}
+
+function sonidoLinterna() {
+  if (sonidoBatman.paused) {
+    sonidoBoton.play();
+    sonidoBatman.play();
+  } else {
+    sonidoBoton.play();
+    sonidoBatman.pause();
+    // reinicia audio
+    sonidoBatman.currentTime = 0;
+  }
+}
